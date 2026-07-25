e15e6f0a4d4d711b6aa7ec84648dfdee10f5c3cc
yumika-one <siegespokal@gmail.com>
2026-07-25 15:09:37 +0900

    更新.10

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='joy-diary-v08-v1';
+const CACHE='joy-diary-v10-v1';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));

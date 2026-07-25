45f621a025ebadbb3a2e62eed8d8b4b7b76cd727
yumika-one <siegespokal@gmail.com>
2026-07-25 13:20:39 +0900

    更新
    表紙を素敵フォントの JOY Diary に変更
    中央リングに文字がはみ出しにくいよう余白を拡大
    7月30日に書く「45日間全体の意図」ページ
    7月31日「準備を楽しむ最終日」
    8月1日〜3日のJOY RETREAT
    Afterの余韻期間
    8月8日のJOY Meeting②
    9月12日のJOY Meeting③
    写真やFuture機能は入れず、今日を意図することに集中

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='joy-diary-step4-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));

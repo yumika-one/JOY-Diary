e21f95fbcabf380c8d5093dc30c0c099ea7a5269
yumika-one <siegespokal@gmail.com>
2026-07-25 15:53:12 +0900

    更新v0.11
    朝、窓を開けたような空気。

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='joy-diary-v10-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='joy-diary-v11-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/art/art-01.jpg','./assets/art/art-02.jpg','./assets/art/art-03.jpg','./assets/art/art-04.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
